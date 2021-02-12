8c41a47a3b19f5e05c23acdb557b97b3072dea01
ramsteven <ramsteven@gmail.com>
2021-02-12 18:00:44 -0500

    adding task

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,30 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(params.require(:user).permit(:name, :description, :twitter, :facebook, :instagram))
-    @user.save
-    redirect_to user_path(@user)
+    if @user.save
+      flash[:nortice] = "User was created succesfully"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:title, :description, :twitter, :facebook, :instagram))
+      flash[:notice] = "User was updated successfully."
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {minimum: 2, maximum: 100}
   validates :description, presence: true, length: {minimum: 2, maximum: 300}
   validates :twitter, presence: true, length: {minimum: 2, maximum: 100}
-  validates :facebook, length: {minimum: 2, maximum: 100}
-  validates :instagram, length: {minimum: 2, maximum: 100}
+  validates :facebook, presence:false
+  validates :instagram, presence: false
 end
