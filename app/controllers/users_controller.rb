aefa1f70bb2455424bc72d3fbd1855960902d78b
ramsteven <ramsteven@gmail.com>
2021-02-12 15:46:34 -0500

    Create user working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   def show
-    byebug
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
+  end
+  
+  def new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit(:name, :description, :twitter, :facebook, :instagram))
+    @user.save
+    redirect_to user_path(@user)
   end
 end
