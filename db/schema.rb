9e1836c8a4747aa907682376f5f37ca6ed32d1d1
ramsteven <ramsteven@gmail.com>
2021-02-22 17:56:53 -0500

    Bcry working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_185105) do
+ActiveRecord::Schema.define(version: 2021_02_22_223817) do
 
   create_table "socials", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_02_18_185105) do
     t.text "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "socials", "users"
